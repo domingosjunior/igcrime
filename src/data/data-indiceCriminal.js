67d0f440d5e0fdd78b53628aae9613b55a78d72b
luciano.lourenco <luciano.lourenco@pgj.rj.gov.br>
2017-08-02 15:42:08 -0300

    Created object with the index database of municipalities

---
+++ src/data/data-indiceCriminal.js
@@ -0,0 +1,71 @@
+var dataIndice = {
+    '2013': {
+        maisViolentas: [
+                            {
+                                nameMunicipio: "Duque de Caxias",
+                                rank: "1º Colocado",
+                                icg: "0,586",
+                                imgMunicipio: "../data/2012/img/mais-violentas/dq-caxias.png",
+                                linkBtn: "linkAqui"
+                            },
+                            {
+                                nameMunicipio: "Nilópolis",
+                                rank: "2º Colocado",
+                                icg: "0,000",
+                                imgMunicipio: "../data/2012/img/mais-violentas/nilopolis.png",
+                                linkBtn: "linkAquiNilopolis"
+                            }
+                        ],
+        maisSeguras: [
+            {
+                nameMunicipio: "Menos violenta Duque de Caxias",
+                rank: "1º Colocado",
+                icg: "0,586",
+                imgMunicipio: "../data/2012/img/mais-violentas/dq-caxias.png",
+                linkBtn: "linkAqui"
+            },
+            {
+                nameMunicipio: "Menos violenta Nilópolis",
+                rank: "2º Colocado",
+                icg: "0,000",
+                imgMunicipio: "../data/2012/img/mais-violentas/nilopolis.png",
+                linkBtn: "linkAquiNilopolis"
+            }
+        ]
+    },
+    '2014': {
+        maisViolentas: [
+            {
+                nameMunicipio: "Duque de Caxias",
+                rank: "2014 1º Colocado",
+                icg: "0,586",
+                imgMunicipio: "../data/2012/img/mais-violentas/dq-caxias.png",
+                linkBtn: "linkAqui"
+            },
+            {
+                nameMunicipio: "2014 Nilópolis",
+                rank: "2014 2º Colocado",
+                icg: "0,000",
+                imgMunicipio: "../data/2012/img/mais-violentas/nilopolis.png",
+                linkBtn: "linkAquiNilopolis"
+            }
+        ],
+        maisSeguras: [
+            {
+                nameMunicipio: "Menos violenta Duque de Caxias",
+                rank: "2014 1º Colocado",
+                icg: "0,586",
+                imgMunicipio: "../data/2012/img/mais-violentas/dq-caxias.png",
+                linkBtn: "linkAqui"
+            },
+            {
+                nameMunicipio: "Menos violenta Nilópolis",
+                rank: "2014 2º Colocado",
+                icg: "0,000",
+                imgMunicipio: "../data/2012/img/mais-violentas/nilopolis.png",
+                linkBtn: "linkAquiNilopolis"
+            }
+        ]
+    },
+
+}
